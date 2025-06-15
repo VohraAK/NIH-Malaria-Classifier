e02cd75892ba7752bf5d3e85bafd95e1337e639f
VohraAK <abdullah.k.vohra@gmail.com>
2025-06-15 16:48:07 +0500

    added website skeleton #2

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="shadow-md">
+      <div className="flex justify-between p-5 items-center">
+        <div className="pl-3 text-2xl">MalarAI</div>
+        <div className="text-md">About</div>
+      </div>
+    </header>
+  );
+}
